f235f8c5e404f684e9305c28b01ea3f0e16e9962
Aditya Kumar Jha <adityajha126@gmail.com>
2019-06-19 08:13:30 +0530

    mongodb compass error

--- index.js
+++ index.js
@@ -1,25 +1,66 @@
 const path = require('path');
- 
+const expressEdge = require('express-edge');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const fileUpload = require("express-fileupload");
+ 
+const Post = require('./database/models/Post');
  
 const app = new express();
  
-app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
  
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+app.use(fileUpload());
+app.use(express.static('public'));
+app.use(expressEdge);
+app.set('views', __dirname + '/views');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+app.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
+});
+ 
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+});
+ 
+app.post("/posts/store", (req, res) => {
+    const {
+        image
+    } = req.files
+ 
+    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+        Post.create({
+            ...req.body,
+            image: `/posts/${image.name}`
+        }, (error, post) => {
+            res.redirect('/');
+        });
+    })
 });
  
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
 });
  
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 });
  
-app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+app.get('/post/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('post', {
+        post
+    })
 });
  
 app.listen(4000, () => {
