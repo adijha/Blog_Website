31f0bdb35a9de33be51f5251d43c9334be39a885
Aditya Kumar Jha <adityajha126@gmail.com>
2019-06-27 20:50:02 +0530

    added user login

---
+++ controllers/loginUser.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt')
+const User = require('../database/models/User')
+ 
+module.exports = (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+    // try to find the user
+    User.findOne({
+        email
+    }, (error, user) => {
+        if (user) {
+            // compare passwords.
+            bcrypt.compare(password, user.password, (error, same) => {
+                if (same) {
+                    req.session.userId = user._id
+                    res.redirect('/')
+                } else {
+                    res.redirect('/auth/login')
+                }
+            })
+        } else {
+            return res.redirect('/auth/login')
+        }
+    })
+}
--- index.js
+++ index.js
@@ -3,21 +3,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
- 
-const createPostController = require('./controllers/createPost')
-const homePageController = require('./controllers/homePage')
-const storePostController = require('./controllers/storePost')
-const getPostController = require('./controllers/getPost')
+const expressSession = require('express-session')
+
+const createPostController = require('./controllers/createPost');
+const homePageController = require('./controllers/homePage');
+const storePostController = require('./controllers/storePost');
+const getPostController = require('./controllers/getPost');
+const createUserController = require("./controllers/createUser");
+const storeUserController = require('./controllers/storeUser');
+const loginController = require("./controllers/login");
+const loginUserController = require('./controllers/loginUser');
  
 const app = new express();
  
-// mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
-//     .then(() => 'You are now connected to Mongo!')
-//     .catch(err => console.error('Something went wrong', err))
+app.use(expressSession({
+    secret: 'secret'
+}));
 
-mongoose.connect(process.env.mongoURI, { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
-    .catch(err => console.error('Something went wrong', err))    
+    .catch(err => console.error('Something went wrong', err))
  
 app.use(fileUpload());
 app.use(express.static("public"));
@@ -35,7 +40,11 @@ app.get("/", homePageController);
 app.get("/post/:id", getPostController);
 app.get("/posts/new", createPostController);
 app.post("/posts/store", storePostController);
+app.get('/auth/login', loginController);
+app.post('/users/login', loginUserController);
+app.get("/auth/register", createUserController);
+app.post("/users/register", storeUserController);
  
 app.listen(4000, () => {
   console.log("App listening on port 4000");
-});
+}); 
