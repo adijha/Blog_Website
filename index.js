d2a82bc61ac99e68a1f1ffe0b434c3d26c92f940
Aditya Kumar Jha <adityajha126@gmail.com>
2019-06-27 21:51:40 +0530

    adi

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const expressEdge = require("express-edge");
 const express = require("express");
+const edge = require("edge.js");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const expressSession = require('express-session')
-
+const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
+const connectFlash = require("connect-flash");
+ 
 const createPostController = require('./controllers/createPost');
 const homePageController = require('./controllers/homePage');
 const storePostController = require('./controllers/storePost');
@@ -16,35 +19,49 @@ const loginUserController = require('./controllers/loginUser');
  
 const app = new express();
  
-app.use(expressSession({
-    secret: 'secret'
-}));
-
-mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
+mongoose.connect(process.env.mongoURI, { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
+ 
+app.use(connectFlash());
+ 
+const mongoStore = connectMongo(expressSession);
+ 
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
  
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(expressEdge);
 app.set('views', __dirname + '/views');
  
+app.use('*', (req, res, next) => {
+    edge.global('auth', req.session.userId)
+    next()
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- 
-const storePost = require('./middleware/storePost')
- 
 app.use('/posts/store', storePost)
+ 
+const storePost = require('./middleware/storePost');
+const auth = require("./middleware/auth");
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated');
+const logoutController = require("./controllers/logout");
  
 app.get("/", homePageController);
 app.get("/post/:id", getPostController);
-app.get("/posts/new", createPostController);
-app.post("/posts/store", storePostController);
-app.get('/auth/login', loginController);
-app.post('/users/login', loginUserController);
-app.get("/auth/register", createUserController);
-app.post("/users/register", storeUserController);
+app.get("/posts/new", auth, createPostController);
+app.post("/posts/store", auth, storePost, storePostController);
+app.get("/auth/login", redirectIfAuthenticated, loginController);
+app.post("/users/login", redirectIfAuthenticated, loginUserController);
+app.get("/auth/register", redirectIfAuthenticated, createUserController);
+app.get("/auth/logout", redirectIfAuthenticated, logoutController);
+app.post("/users/register", redirectIfAuthenticated, storeUserController);
  
 app.listen(4000, () => {
   console.log("App listening on port 4000");
-}); 
+});
